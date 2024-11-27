59d6fb4e27c365868e90aa3a3db79e4434e66d07
Nero3ooo <76692751+Nero3ooo@users.noreply.github.com>
2024-11-27 13:51:16 +0100

    extract download to method and add encoding for '?'

--- audioDownloader.py
+++ audioDownloader.py
@@ -30,6 +30,7 @@ path = parsed_uri.path
 
 def loadEpisode(path):
     r = requests.get(url+path)
+
     soup = BeautifulSoup(r.content, 'html.parser')
 
     script = soup.find(id='__NEXT_DATA__')
@@ -39,24 +40,42 @@ def loadEpisode(path):
 
     # load from first episode
     if 'item' in data:
-        # get download link
-        link = data['item']['audios'][0]['url']
+        downloadTitle(data['item'])
+
+        # recursively download next episodes
+        next_episode = data['item']['nextEpisode']
+        if not next_episode is None:
+            loadEpisode(next_episode['path'])
+    
+    # load from series site, does not work for the whole series
+    else:
+        nodes = data['result']['items']['nodes']
+        for node in nodes:
+            downloadTitle(node)
+            # print(node['title'])
+            # print(node['audios'][0]['url'])
+
+def downloadTitle(itemNode):
+     # get download link
+        link = itemNode['audios'][0]['url']
         link = link + "?download=true"
 
         # get file name
-        filename = data['item']['title']
+        filename = itemNode['title']
         filename = filename.replace('"','')
         filename = filename.replace('/','-')
         filename = filename.replace(':',' -')
         filename = filename.replace('\t',' ')
         filename = filename.replace('|','-')
         filename = filename.replace('–', '-')
+        filename = filename.replace('?', '')
         filename = filename + '.mp3'
 
         # get folder names
-        title = data['item']['programSet']['title']
+        title = itemNode['programSet']['title']
         title = title.replace('"', '')
         title = title.replace('/', '')
+        title = title.replace('?', '')
         title = title.replace(': ', '-')
         title = title.replace(' – ', '-')
         title = title.replace(' - ', '-')
@@ -75,22 +94,11 @@ def loadEpisode(path):
         if len(title) >= 2:
             filename = os.path.join(cwd, title[0], title[1], filename)
         
+        print(filename)
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         
         # download episode
         wget.download(link, filename)
         print()
-
-        # recursively download next episodes
-        next_episode = data['item']['nextEpisode']
-        if not next_episode is None:
-            loadEpisode(next_episode['path'])
-    
-    # load from series site, does not work for now
-    else:
-        nodes = data['result']['items']['nodes']
-        for node in nodes:
-            print(node['title'])
-            #print(node['audios'][0]['url'])
 
 loadEpisode(path)
