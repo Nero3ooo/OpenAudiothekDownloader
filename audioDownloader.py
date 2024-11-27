6dc99cc351672d5f1c5dda7692d47eb233c09c33
Nero3ooo <76692751+Nero3ooo@users.noreply.github.com>
2024-11-27 16:38:44 +0100

    add experimental api call
    Use https://api.ardaudiothek.de/graphql to get items of show instead of list from site

--- audioDownloader.py
+++ audioDownloader.py
@@ -21,7 +21,13 @@ parser = argparse.ArgumentParser(description='Downloader for ARD Audiothek Audio
 parser.add_argument('url', type=str,
                     help='Url of first episode')
 
+parser.add_argument('--experimentalShow', action=argparse.BooleanOptionalAction, help='If you want to download bigger shows, you can try this. There may be duplicates.')
+parser.add_argument('--dryRun', action=argparse.BooleanOptionalAction, help='If you just want to list what will be downloaded and where it will be stored.')
+
 args = parser.parse_args()
+
+experimentalShow = args.experimentalShow
+dryRun = args.dryRun
 
 # get url and path from uri
 parsed_uri = urlparse(args.url)
@@ -29,34 +35,80 @@ url = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
 path = parsed_uri.path
 
 def loadEpisode(path):
-    r = requests.get(url+path)
+    if not experimentalShow:
+        r = requests.get(url+path)
 
-    soup = BeautifulSoup(r.content, 'html.parser')
+        soup = BeautifulSoup(r.content, 'html.parser')
 
-    script = soup.find(id='__NEXT_DATA__')
-    json_object = json.loads(script.contents[0])
-    # print(json.dumps(json_object, indent=4))
-    data = json_object['props']['pageProps']['initialData']['data']
+        script = soup.find(id='__NEXT_DATA__')
+        json_object = json.loads(script.contents[0])
+        # print(json.dumps(json_object, indent=4))
+        data = json_object['props']['pageProps']['initialData']['data']
 
-    # load from first episode
-    if 'item' in data:
-        downloadTitle(data['item'])
+        # load from first episode
+        if 'item' in data:
+            downloadTitle(data['item'])
 
-        # recursively download next episodes
-        next_episode = data['item']['nextEpisode']
-        if not next_episode is None:
-            loadEpisode(next_episode['path'])
+            # recursively download next episodes
+            next_episode = data['item']['nextEpisode']
+            if not next_episode is None:
+                loadEpisode(next_episode['path'])
+        
+        # load from series site, does not work for the whole series
+        else:
+            nodes = data['result']['items']['nodes']
+            for node in nodes:
+                downloadTitle(node)
     
-    # load from series site, does not work for the whole series
+    # !! Experimental with GraphQL !!
     else:
-        nodes = data['result']['items']['nodes']
-        for node in nodes:
-            downloadTitle(node)
-            # print(node['title'])
-            # print(node['audios'][0]['url'])
+        # get programSetId from path
+        programSetId = path.split("/")[-2]
+
+        # Provide a GraphQL query
+        query = """
+        query Query($programSetId: ID!) {
+            programSet(id: $programSetId) {
+                items {
+                    nodes {
+                        title
+                        audios {
+                            url
+                        }
+                        programSet {
+                            title  
+                        }
+                    }
+                }
+            }
+        }
+        """
+
+        # Define the programSetId 
+        variables = { 
+            "programSetId": programSetId 
+        }
+
+        # Define the request headers 
+        headers = { 
+            "Content-Type": "application/json" 
+        }
+
+        # Execute the query
+        response = requests.post("https://api.ardaudiothek.de/graphql", json={"query": query, "variables": variables}, headers=headers)
+        
+        # Check the response status code 
+        if response.status_code == 200: 
+            # Parse the JSON response 
+            data = response.json() 
+            nodes = data['data']['programSet']['items']['nodes']
+            #print(nodes)
+            for node in nodes:
+                if len(node['audios'])>0:
+                    downloadTitle(node)
 
 def downloadTitle(itemNode):
-     # get download link
+        # get download link
         link = itemNode['audios'][0]['url']
         link = link + "?download=true"
 
@@ -98,7 +150,8 @@ def downloadTitle(itemNode):
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         
         # download episode
-        wget.download(link, filename)
+        if not dryRun:
+            wget.download(link, filename)
         print()
 
 loadEpisode(path)
